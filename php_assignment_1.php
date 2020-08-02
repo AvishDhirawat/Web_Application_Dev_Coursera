9623580d685d08ad2bf2d3215ec3d112afd29666
Avish Dhirawat <31956212+Avishdhirawat0@users.noreply.github.com>
2020-08-02 23:25:03 +0530

    Add files via upload

---
+++ php_assignment_1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Charles Severance PHP</title>
+  </head>
+  <body>
+    <h1>Avish Dhirawat PHP, Charles Severance PHP</h1>
+    <p style="font-family:Courier new">ASCII ART:</p><br>
+    <pre style = "margin-left:4em">
+          **
+        **   **
+      **       **
+      ** * * * **
+      **       **
+      **       **
+    </pre><br>
+    <?php
+    $value = hash('sha256', 'AVISH DHIRAWAT');
+    echo "SHA256 of the name AVISH DHIRAWAT is :".$value;
+     ?>
+     <br><br><br>
+     <a href="http://localhost/Coursera/check.php">Click here to check error settings!</a><br>
+     <a href="http://localhost/Coursera/fail.php">Click here to cause an error!</a>
+  </body>
+</html>
