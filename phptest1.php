e25c681e748c0ae147cd93ff710f854762570f86
Avish Dhirawat <31956212+Avishdhirawat0@users.noreply.github.com>
2020-08-02 21:21:28 +0530

    Add files via upload

---
+++ phptest1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>phpTest</title>
+  </head>
+  <body>
+    <h1>This is the H1 header</h1>
+    <p>
+      We are in the paragraph now.<br>
+      Let's see how PHP works.<br>
+      <?php
+      $x = 5;
+      $y = 8;
+      $answer = $x*$y;
+      echo "The Sum of ".$x." and ".$y." is ".$answer."\n";
+       ?>
+    </p>
+  </body>
+</html>
